c23b4d02312cdae095579061f60ee69cca4d89d4
Denis Borisovich Fishman <106389581+fishman123456@users.noreply.github.com>
2024-05-14 13:38:03 +0700

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,61 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.DatabaseServices.Filters;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autocad_Get_lenght_polyline_03_05_2024
+{
+    public class Class1
+    {
+        [CommandMethod("U84PolyLenght")]
+        public void SelectAllPolylineByLayer()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            Database db = doc.Database;
+
+           StringBuilder Cable = new StringBuilder();
+            Transaction tr = db.TransactionManager.StartTransaction();
+            using (tr)
+            {
+                try
+                {
+                    PromptSelectionOptions opts = new PromptSelectionOptions();
+                    opts.MessageForAdding = "Выберите кабели: ";
+                    PromptSelectionResult res = ed.GetSelection(opts);
+                    // Do nothing if selection is unsuccessful
+                    if (res.Status != PromptStatus.OK)
+                        return;
+                    SelectionSet selSet = res.Value;
+                    // добавляем в массив выбранные обьекты
+                    ObjectId[] idArray = selSet.GetObjectIds();
+                    foreach (ObjectId id in idArray)
+                    {
+
+                        DBObject obj = tr.GetObject(id, OpenMode.ForRead);
+                        // выбирам из коллекции атрибутов по ссылкам на блоки
+                        Polyline lwp = obj as Polyline; // Get the selected polyline during runtime
+                       Cable.Append( lwp.Layer.ToString() + ";" );
+                        Cable.Append(lwp.Length.ToString() + ";" );
+                        Cable.Append("\n");
+                    }
+                    SaveCsv saveCsv = new SaveCsv();
+                    saveCsv.saveCsv(Cable.ToString());
+                }
+                catch (System.Exception ex)
+                {
+                    ed.WriteMessage(ex.Message);
+                    tr.Abort();
+                }
+            }
+            tr.Commit();
+        }
+    }
+}
---
+++ SaveCSV.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Autocad_Get_lenght_polyline_03_05_2024
+{
+    public class SaveCsv
+    {
+       
+        public async void saveCsv(string text)
+        {
+            // для русских букв в екселе подключить NUGET
+            // NuGet\Install-Package
+            // Вот нужный пакет 
+
+            // System.Text.Encoding.CodePages
+
+            // Вот нужный пакет 
+            // -Version 8.0.0
+            // Register the CodePages encoding provider at application startup to enable using single and double byte encodings.
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            // проверка по текущей дате
+            CheckDateWork.CheckDate();
+            // открываем диалог для сохранения файла в поток
+            System.Windows.Forms.SaveFileDialog saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            saveFileDialog1.Filter = "Text files(*.csv)|*.csv|All files(*.*)|*.*";
+            // если не сохраняем то выходим
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string path = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            try
+            {
+                // полная перезапись файла  new FileStream(path, FileMode.OpenOrCreate), Encoding.GetEncoding(1251))
+                using (StreamWriter writer = new StreamWriter(path, false, Encoding.GetEncoding(1251)))
+                {
+                    // асинхронная перезапись  файла
+                    await writer.WriteAsync(text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
